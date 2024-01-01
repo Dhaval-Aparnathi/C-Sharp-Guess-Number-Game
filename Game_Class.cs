c2e563ba53fd84a692eaea5d66460a36c96c2d1d
Dhaval-Aparnathi <137690550+Dhaval-Aparnathi@users.noreply.github.com>
2024-01-01 21:21:17 +0530

    Add files via upload

---
+++ Game_Class.cs
@@ -0,0 +1,52 @@
+﻿namespace Guess_The_Random_Number
+{
+    internal class Game_Class
+    {
+        private int UserInput;
+        private int CompInput;
+        private int Counter = 0;
+        public Game_Class() 
+        {
+            Random random = new Random();
+            CompInput = random.Next(1, 100);
+        }
+        public void SetUserInput(int UserInput)
+        {
+            this.UserInput = UserInput;
+        }
+        public int IsRightGuess()
+        {
+            if (CompInput > UserInput)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                SetCounter();
+                Console.WriteLine("\nWrong Guess !!! Hint => Random Number is Greater...");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n_____________________________________________________________________\n");
+                return 1;
+            }
+            else if (CompInput < UserInput)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                SetCounter();
+                Console.WriteLine("\nWrong Guess !!! Hint => Random Number is Smaller...");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n_____________________________________________________________________\n");
+                return 2;
+            }
+            else
+            {
+                SetCounter();
+                return 3;
+            }
+        }
+        public void SetCounter()
+        {
+            Counter++;
+        }
+        public void GetCounter()
+        {
+            Console.WriteLine("The number of Guesses => " + Counter);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Guess_The_Random_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game_Class obj = new Game_Class();
+            int n;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("----> NUMBER GUESSING GAME (1 - 100) <----");
+            play:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\nGuess the Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            obj.SetUserInput(n);
+            if (obj.IsRightGuess() == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n----> You Won <----\n");
+                obj.GetCounter();
+            }
+            else
+            {
+                goto play;
+            }
+            Console.ReadKey();
+        }
+    }    
+}
